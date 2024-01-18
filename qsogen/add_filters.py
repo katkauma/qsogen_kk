f155f37d1433ea565bc2c2c405dc3e0815458028
katkauma <katherine.kauma@gmail.com>
2024-01-18 14:50:28 +0000

    separated model_colours file. created a .json file with filter zeropoints and other info. created add_filters.py to generate new filter info

---
+++ qsogen/add_filters.py
@@ -0,0 +1,141 @@
+import numpy as np
+from scipy.integrate import simps
+import os
+import json
+import pprint
+import glob
+
+install_path = os.path.dirname(os.path.abspath(__file__))
+
+
+
+files = glob.glob(install_path+'/filters/*.filter')
+flist = [item.split('/')[-1][:-7] for item in files]
+
+
+def produce_zeropoints(system='Vega',
+                       filters=flist):
+    """Produce the Vega and AB zero points for filters listed in all_filters variable.
+    Zero points are pre-computed to save time.
+    If you want to compute model photometry in additional filters, first use
+    this function to compute their zeropoints and add to dictionaries above.
+    """
+
+    waves, responses = [], []
+    for band in filters:
+        waves.append(wavarrs[band])
+        responses.append(resparrs[band])
+    #print(system + '_zeropoints = dict(')
+    zp_dict = {}
+
+    if system == 'Vega':
+        wav_Vega, flux_Vega = np.genfromtxt('data/vega_2007.lis', unpack=True)
+        # Vega spectrum
+        fluxes = [np.interp(wav, wav_Vega, flux_Vega) for wav in waves]
+
+        for i in range(len(filters)):
+            F = simps(waves[i]*responses[i]*fluxes[i], waves[i])
+            zp_dict[filters[i]+'_Vega']=float('{:.6e}'.format(F))
+            #print('    ' + filters[i] + '_Vega={:.6e},'.format(F))
+
+    elif system == 'AB':
+        const = 0.1088544752  # 3631Jy in erg/s/cm2/A
+        # AB system has constant f_nu, so convert to f_lambda
+        for i in range(len(filters)):
+            F = const*simps(waves[i]**(-1)*responses[i], waves[i])
+            zp_dict[filters[i]+'_AB']=float('{:.6e}'.format(F))
+            #print('    ' + filters[i] + '_AB={:.6e},'.format(F))
+    else:
+        raise Exception('System must be "Vega" or "AB"')
+
+    return zp_dict
+       
+def produce_pivotwv(filters=flist):
+    """Produce the inverse squared pivot wavelength for each filter (lambda_pivot**-2). Used in converting between fnu and flambda in get_mags. 
+    """
+
+    waves, responses = [], []
+    for band in filters:
+        waves.append(wavarrs[band])
+        responses.append(resparrs[band])
+    #print('pivotwv = dict(')
+    pivot_dict ={}
+
+    for i in range(len(filters)):
+        F = np.sqrt(simps(waves[i]*responses[i], waves[i])/simps(responses[i]*waves[i]**-1,waves[i]))
+        pivot_dict[filters[i]]=float('{:.7g}'.format(F))
+        
+        
+        #print('    ' + filters[i] + '={:.7g},'.format(F))
+
+    return pivot_dict
+    #print(')')
+
+def vega2ab(vega_zp,ab_zp,filters=flist):
+    vega2ab_dict = {}
+    for band in filters:
+        vega2ab_dict[band]= float(round(-2.5*np.log10(vega_zp[band+'_Vega']/ab_zp[band+'_AB']),4))
+    return vega2ab_dict
+
+
+if __name__ == '__main__':
+    #check files
+    files = glob.glob(install_path+'/filters/*.filter')
+    flist = [item.split('/')[-1][:-7] for item in files]
+    
+    
+    with open(install_path+'/filterinfo.json','r') as file:
+        filterinfo = json.load(file)
+    newfilters = list(set(flist)-set(filterinfo['Pivot_wv'].keys()))
+    missingfilters = list(set(filterinfo['Pivot_wv'].keys())-set(flist)) # filters in filterinfo.json that do not have response curves
+    
+    filterlist = newfilters
+    
+
+    wavarrs, resparrs = dict(), dict()
+    for band in flist:
+        try:
+            wavarr, response = np.genfromtxt(install_path+'/filters/'+band+'.filter', unpack=True)
+        except OSError:
+            wavarr, response = np.genfromtxt(install_path+'/filters/'+band+'.filter', unpack=True)
+        wavarrs[band] = wavarr
+        resparrs[band] = response
+
+    #read in the existing filterinfo.py file
+
+    # get new files
+    
+    
+    vega = produce_zeropoints('Vega',filters=filterlist)
+    ab = produce_zeropoints('AB',filters=filterlist)
+    pivot = produce_pivotwv(filters=filterlist)
+    vega2ab = vega2ab(vega_zp=vega,ab_zp=ab,filters=filterlist)
+    
+    #merge with the existing list
+    filterinfo['Vega_zeropoints'].update(vega)
+    filterinfo['AB_zeropoints'].update(ab)
+    filterinfo['Pivot_wv'].update(pivot)
+    filterinfo['Vega_2_AB'].update(vega2ab)
+    
+    for band in missingfilters:
+        del filterinfo['Vega_zeropoints'][band+'_Vega']
+        del filterinfo['AB_zeropoints'][band+'_AB']
+        del filterinfo['Pivot_wv'][band]
+        del filterinfo['Vega_2_AB'][band]
+        
+                  
+    #print('Information in filterinfo.json: \n')
+    #pprint.pprint(filterinfo,indent=4)
+    print('Updated or added the following filters in filterinfo.json:')
+    for band in filterlist:
+        print('\t'+band)
+    
+    print('\nRemoved the following filters with no transmission file from filterinfo.json:')
+    for band in missingfilters:
+        print('\t'+band)
+    print('\n')
+    with open("filterinfo.json","w") as file:
+        json.dump(filterinfo,file,indent=4)
+    
+
+
--- qsogen/model_colours.py
+++ qsogen/model_colours.py
@@ -40,186 +40,27 @@ import numpy as np
 from scipy.integrate import simps
 from qsogen.qsosed import Quasar_sed
 import os
+import json
 
 install_path = os.path.dirname(os.path.abspath(__file__))
 
-all_filters = ['GALEX_NUV','GALEX_FUV',
-               
-                'SDSS_u','SDSS_g','SDSS_r','SDSS_i','SDSS_z',
-                
-                'DECam_u','DECam_g','DECam_r','DECam_i','DECam_z','DECam_Y',
-                
-                'HSC_g','HSC_r','HSC_r2','HSC_i','HSC_i2','HSC_z','HSC_Y',
-                
-                'LSST_u','LSST_g','LSST_r','LSST_i','LSST_z','LSST_y',
-                
-                'Euclid_Y','Euclid_J','Euclid_H',
-                
-                'UKIDSS_Z','UKIDSS_Y', 'UKIDSS_J', 'UKIDSS_H','UKIDSS_K',
-                
-                'VISTA_Z','VISTA_Y','VISTA_J','VISTA_H','VISTA_Ks',
-                
-                'WISE_W1','WISE_W2','WISE_W3','WISE_W4',
-                
-                'BOK_g','BOK_r',
-                
-                'MzLS_z']
 
-# assume 2007 Vega spectrum has zero magnitude in all bands
-Vega_zeropoints = dict(
-    GALEX_NUV_Vega=4.638133e-01,
-    GALEX_FUV_Vega=9.411333e-02,
-    SDSS_u_Vega=7.986921e-04,
-    SDSS_g_Vega=1.071783e-02,
-    SDSS_r_Vega=8.455695e-03,
-    SDSS_i_Vega=4.624980e-03,
-    SDSS_z_Vega=6.676051e-04,
-    DECam_u_Vega=8.177943e-04,
-    DECam_g_Vega=1.903633e-02,
-    DECam_r_Vega=1.684878e-02,
-    DECam_i_Vega=1.283454e-02,
-    DECam_z_Vega=9.031628e-03,
-    DECam_Y_Vega=3.316464e-03,
-    HSC_g_Vega=1.497881e-02,
-    HSC_r_Vega=1.150771e-02,
-    HSC_r2_Vega=1.270496e-02,
-    HSC_i_Vega=7.319484e-03,
-    HSC_i2_Vega=7.188806e-03,
-    HSC_z_Vega=2.760566e-03,
-    HSC_Y_Vega=1.945597e-03,
-    LSST_u_Vega=1.782632e-02,
-    LSST_g_Vega=1.423635e-01,
-    LSST_r_Vega=9.468552e-02,
-    LSST_i_Vega=5.861893e-02,
-    LSST_z_Vega=3.521813e-02,
-    LSST_y_Vega=1.643208e-02,
-    Euclid_Y_Vega=7.743718e-03,
-    Euclid_J_Vega=7.092825e-03,
-    Euclid_H_Vega=4.253855e-03,
-    UKIDSS_Z_Vega=1.284425e-03,
-    UKIDSS_Y_Vega=1.119803e-03,
-    UKIDSS_J_Vega=1.179108e-03,
-    UKIDSS_H_Vega=1.409765e-03,
-    UKIDSS_K_Vega=6.859715e-04,
-    VISTA_Z_Vega=7.195167e-03,
-    VISTA_Y_Vega=4.610067e-03,
-    VISTA_J_Vega=5.395487e-03,
-    VISTA_H_Vega=5.122664e-03,
-    VISTA_Ks_Vega=2.727685e-03,
-    WISE_W1_Vega=1.810656e-03,
-    WISE_W2_Vega=1.156727e-03,
-    WISE_W3_Vega=3.901671e-04,
-    WISE_W4_Vega=4.489671e-05,
-    BOK_g_Vega=1.535034e-02,
-    BOK_r_Vega=9.710702e-03,
-    MzLS_z_Vega=4.706509e-03,
-)
-AB_zeropoints = dict(
-    GALEX_NUV_AB=2.143184e+00,
-    GALEX_FUV_AB=6.682505e-01,
-    SDSS_u_AB=1.857387e-03,
-    SDSS_g_AB=9.721695e-03,
-    SDSS_r_AB=9.641594e-03,
-    SDSS_i_AB=6.418155e-03,
-    SDSS_z_AB=1.078642e-03,
-    DECam_u_AB=1.104899e-03,
-    DECam_g_AB=1.742508e-02,
-    DECam_r_AB=1.991803e-02,
-    DECam_i_AB=1.868959e-02,
-    DECam_z_AB=1.455719e-02,
-    DECam_Y_AB=5.599771e-03,
-    HSC_g_AB=1.364416e-02,
-    HSC_r_AB=1.313723e-02,
-    HSC_r2_AB=1.450169e-02,
-    HSC_i_AB=1.044899e-02,
-    HSC_i2_AB=1.033459e-02,
-    HSC_z_AB=4.426118e-03,
-    HSC_Y_AB=3.224177e-03,
-    LSST_u_AB=3.219424e-02,
-    LSST_g_AB=1.298923e-01,
-    LSST_r_AB=1.083751e-01,
-    LSST_i_AB=8.188370e-02,
-    LSST_z_AB=5.619859e-02,
-    LSST_y_AB=2.718541e-02,
-    Euclid_Y_AB=1.454023e-02,
-    Euclid_J_AB=1.846444e-02,
-    Euclid_H_AB=1.634865e-02,
-    UKIDSS_Z_AB=2.060514e-03,
-    UKIDSS_Y_AB=1.972653e-03,
-    UKIDSS_J_AB=2.739415e-03,
-    UKIDSS_H_AB=4.897400e-03,
-    UKIDSS_K_AB=3.844725e-03,
-    VISTA_Z_AB=1.152809e-02,
-    VISTA_Y_AB=8.005087e-03,
-    VISTA_J_AB=1.255071e-02,
-    VISTA_H_AB=1.793209e-02,
-    VISTA_Ks_AB=1.460059e-02,
-    WISE_W1_AB=2.134738e-02,
-    WISE_W2_AB=2.450814e-02,
-    WISE_W3_AB=4.859685e-02,
-    WISE_W4_AB=2.006357e-02,
-    BOK_g_AB=1.399249e-02,
-    BOK_r_AB=1.142863e-02,
-    MzLS_z_AB=7.589110e-03,)
+# load the zeropoints and pivot wavelengths from filterinfo.json
+with open(install_path+'/filterinfo.json','r') as file:
+    filterinfo = json.load(file)
 
-
+Vega_zeropoints=filterinfo['Vega_zeropoints']
+AB_zeropoints = filterinfo['AB_zeropoints']
+pivotwv = filterinfo['Pivot_wv']
 
 zeropoints = {**Vega_zeropoints, **AB_zeropoints}
 
 
-pivotwv2 = dict(
-    GALEX_NUV=5.293611e+06,
-    GALEX_FUV=2.356469e+06,
-    SDSS_u=1.264872e+07,
-    SDSS_g=2.211349e+07,
-    SDSS_r=3.813791e+07,
-    SDSS_i=5.609555e+07,
-    SDSS_z=8.004292e+07,
-    DECam_u=1.454743e+07,
-    DECam_g=2.312162e+07,
-    DECam_r=4.118621e+07,
-    DECam_i=6.106772e+07,
-    DECam_z=8.406795e+07,
-    DECam_Y=9.793339e+07,
-    HSC_g=2.295522e+07,
-    HSC_r=3.861266e+07,
-    HSC_r2=3.859520e+07,
-    HSC_i=5.911890e+07,
-    HSC_i2=5.980280e+07,
-    HSC_z=7.926532e+07,
-    HSC_Y=9.548639e+07,
-    LSST_u=1.354273e+07,
-    LSST_g=2.287005e+07,
-    LSST_r=3.866125e+07,
-    LSST_i=5.673519e+07,
-    LSST_z=7.543031e+07,
-    LSST_y=9.467403e+07,
-    Euclid_Y=1.173135e+08,
-    Euclid_J=1.859249e+08,
-    Euclid_H=3.083767e+08,
-    UKIDSS_Z=7.790447e+07,
-    UKIDSS_Y=1.063794e+08,
-    UKIDSS_J=1.562799e+08,
-    UKIDSS_H=2.674812e+08,
-    UKIDSS_K=4.865715e+08,
-    VISTA_Z=7.701424e+07,
-    VISTA_Y=1.041830e+08,
-    VISTA_J=1.570095e+08,
-    VISTA_H=2.703097e+08,
-    VISTA_Ks=4.608644e+08,
-    WISE_W1=1.148993e+09,
-    WISE_W2=2.153550e+09,
-    WISE_W3=1.579444e+10,
-    WISE_W4=4.979197e+10,
-    BOK_g=2.274303e+07,
-    BOK_r=4.083468e+07,
-    MzLS_z=8.464611e+07,
-)
 
 ###########################################################
 
 wavarrs, resparrs = dict(), dict()
-for band in all_filters:
+for band in pivotwv.keys():
     try:
         wavarr, response = np.genfromtxt(install_path+'/filters/'+band+'.filter', unpack=True)
     except OSError:
@@ -411,7 +252,6 @@ def get_mags(redshifts,
     return(model_mags)
 
 
-
 def sed2mags(filters, waves, fluxes, responses):
 
         mags = np.full(len(waves), np.nan)
@@ -480,136 +320,10 @@ def get_fluxes(redshifts,
     elif units=='flam':
         pivlam2=np.array([pivotwv2[band.replace('_AB','').replace('_Vega','')] for band in filters])
         
-        flam = 2.9982e-5*fnu/pivlam2
+        flam = 2.9982e-5*fnu/(pivlam*pivlam)
         return flam
             
     else:
         raise ValueError("'units' keyword must be 'flam' or 'fnu")
 
-    
-
-def produce_zeropoints(system='Vega',
-                       filters=all_filters):
-    """Produce the Vega and AB zero points for filters listed in all_filters variable.
-    Zero points are pre-computed to save time.
-    If you want to compute model photometry in additional filters, first use
-    this function to compute their zeropoints and add to dictionaries above.
-    """
-
-    waves, responses = [], []
-    for band in filters:
-        waves.append(wavarrs[band])
-        responses.append(resparrs[band])
-    print(system + '_zeropoints = dict(')
-
-    if system == 'Vega':
-        wav_Vega, flux_Vega = np.genfromtxt('data/vega_2007.lis', unpack=True)
-        # Vega spectrum
-        fluxes = [np.interp(wav, wav_Vega, flux_Vega) for wav in waves]
-
-        for i in range(len(filters)):
-            F = simps(waves[i]*responses[i]*fluxes[i], waves[i])
-            print('    ' + filters[i] + '_Vega={:.6e},'.format(F))
-
-    elif system == 'AB':
-        const = 0.1088544752  # 3631Jy in erg/s/cm2/A
-        # AB system has constant f_nu, so convert to f_lambda
-        for i in range(len(filters)):
-            F = const*simps(waves[i]**(-1)*responses[i], waves[i])
-            print('    ' + filters[i] + '_AB={:.6e},'.format(F))
-    else:
-        raise Exception('System must be "Vega" or "AB"')
-    print(')')
-    
-
-    
-    
-def produce_pivotwv2(filters=all_filters):
-    """Produce the inverse squared pivot wavelength for each filter (lambda_pivot**-2). Used in converting between fnu and flambda in get_mags. 
-    If you want to compute model photometry in additional filters, first use
-    this function to compute their normalisation constants and add to dictionaries above.
-    """
-
-    waves, responses = [], []
-    for band in filters:
-        waves.append(wavarrs[band])
-        responses.append(resparrs[band])
-    print('pivotwv2 = dict(')
-
-
-    for i in range(len(filters)):
-        F = (simps(waves[i]*responses[i], waves[i])/simps(responses[i]*waves[i]**-1,waves[i]))
-        
-        
-        print('    ' + filters[i] + '={:.6e},'.format(F))
-        
-    print(')')
-
-
-
-if __name__ == '__main__':
-    produce_zeropoints('Vega')
-    produce_zeropoints('AB')
-    
-    produce_pivotwv2()
-    
-    print(
-        'Derived delta_m = m_AB - m_Vega conversions are as follows, assuming')
-    print(
-        'Vega has zero magnitude in all bands (consistent with Hewett+ 2006)')
-    for band in AB_zeropoints:
-        band = band.replace('_AB', '')
-        print(band,
-              -2.5*np.log10(zeropoints[band+'_Vega']/zeropoints[band+'_AB']))
-
-"""
-Derived delta_m = m_AB - m_Vega conversions are as follows, assuming
-Vega has zero magnitude in all bands (consistent with Hewett+ 2006)
-GALEX_NUV 1.6617906508900804
-GALEX_FUV 2.1282203802545143
-SDSS_u 0.916307531970178
-SDSS_g -0.10591218115027329
-SDSS_r 0.1424988257715102
-SDSS_i 0.35575085540603535
-SDSS_z 0.5208942026802013
-DECam_u 0.32669625325466756
-DECam_g -0.09602111037243544
-DECam_r 0.18169480639014704
-DECam_i 0.40804866596053224
-DECam_z 0.5182787728252919
-DECam_Y 0.5687374491977623
-HSC_g -0.10132627097174592
-HSC_r 0.14378723616842076
-HSC_r2 0.1436132874753628
-HSC_i 0.3864846194174701
-HSC_i2 0.3940912199785226
-HSC_z 0.5125621330816216
-HSC_Y 0.5484149690829487
-LSST_u 0.6417911975387529
-LSST_g -0.09953812415364369
-LSST_r 0.14661485653583178
-LSST_i 0.36289892923935885
-LSST_z 0.5073978180142668
-LSST_y 0.5466083665474974
-Euclid_Y 0.6840543681829497
-Euclid_J 1.0387922398669232
-Euclid_H 1.461748035493578
-UKIDSS_Z 0.5131670489550898
-UKIDSS_Y 0.6147726766033728
-UKIDSS_J 0.9152606087248839
-UKIDSS_H 1.3520471317250795
-UKIDSS_K 1.8713980229183953
-VISTA_Z 0.5117911999718673
-VISTA_Y 0.5991470475407443
-VISTA_J 0.9165941107322068
-VISTA_H 1.3603375970963005
-VISTA_Ks 1.8214404755932185
-WISE_W1 2.6787715820675913
-WISE_W2 3.3151986991103968
-WISE_W3 5.238393684944
-WISE_W4 6.625484235337513
-BOK_g -0.10055748514168539
-BOK_r 0.17685886485765456
-MzLS_z 0.518729885576661
-
-"""
+ 
